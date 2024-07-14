6853f7a82e26029d26cd1c127944a857e4e5589c
Ohkubo Kohei <kuboon@trick-with.net>
2024-07-14 21:02:22 +0900

    fix frontend

--- src/frontend.ts
+++ src/frontend.ts
@@ -1,23 +1,21 @@
-export function getBufFromImgElem(img: HTMLImageElement): ArrayBufferLike {
-  const canvas = document.createElement("canvas");
-  const ctx = canvas.getContext("2d");
-  if (!ctx) {
-    throw new Error("Could not get 2d context from canvas");
+export async function getArrayBufferFrom(something: HTMLImageElement | HTMLInputElement | URL | string | Blob): Promise<ArrayBuffer> {
+  let blob: Blob | undefined;
+  if (something instanceof HTMLImageElement) {
+    blob = await fetch(something.src).then(x => x.blob());
+  } else if (something instanceof HTMLInputElement) {
+    blob = something.files![0];
+  } else if (something instanceof URL) {
+    blob = await fetch(something.href).then(x => x.blob());
+  }else if (typeof something === "string") {
+    const url = URL.parse(something);
+    if(url) {
+      blob = await fetch(url.href).then(x => x.blob());
+    }
+  } else if (something instanceof Blob) {
+    blob = something;
   }
-  canvas.width = img.width;
-  canvas.height = img.height;
-  ctx.drawImage(img, 0, 0);
-  return ctx.getImageData(0, 0, img.width, img.height).data.buffer;
-}
-export function getBufFromFileInput(
-  fileInput: HTMLInputElement,
-): Promise<ArrayBuffer> {
-  const file = fileInput.files![0];
-  return new Promise<ArrayBuffer>((resolve) => {
-    const reader = new FileReader();
-    reader.onload = () => {
-      resolve(reader.result as ArrayBuffer);
-    };
-    reader.readAsArrayBuffer(file);
-  });
+  if(!blob) {
+    throw new Error("Invalid argument. Expected HTMLImageElement | HTMLInputElement(type=file) | URL | string | Blob");
+  }
+  return blob.arrayBuffer();
 }
