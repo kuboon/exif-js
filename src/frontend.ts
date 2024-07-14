8955891ce08adeb476d50c7cfa1538f2d268ad75
Ohkubo Kohei <kuboon@trick-with.net>
2024-07-14 21:03:49 +0900

    deno fmt

--- src/frontend.ts
+++ src/frontend.ts
@@ -1,21 +1,25 @@
-export async function getArrayBufferFrom(something: HTMLImageElement | HTMLInputElement | URL | string | Blob): Promise<ArrayBuffer> {
+export async function getArrayBufferFrom(
+  something: HTMLImageElement | HTMLInputElement | URL | string | Blob,
+): Promise<ArrayBuffer> {
   let blob: Blob | undefined;
   if (something instanceof HTMLImageElement) {
-    blob = await fetch(something.src).then(x => x.blob());
+    blob = await fetch(something.src).then((x) => x.blob());
   } else if (something instanceof HTMLInputElement) {
     blob = something.files![0];
   } else if (something instanceof URL) {
-    blob = await fetch(something.href).then(x => x.blob());
-  }else if (typeof something === "string") {
+    blob = await fetch(something.href).then((x) => x.blob());
+  } else if (typeof something === "string") {
     const url = URL.parse(something);
-    if(url) {
-      blob = await fetch(url.href).then(x => x.blob());
+    if (url) {
+      blob = await fetch(url.href).then((x) => x.blob());
     }
   } else if (something instanceof Blob) {
     blob = something;
   }
-  if(!blob) {
-    throw new Error("Invalid argument. Expected HTMLImageElement | HTMLInputElement(type=file) | URL | string | Blob");
+  if (!blob) {
+    throw new Error(
+      "Invalid argument. Expected HTMLImageElement | HTMLInputElement(type=file) | URL | string | Blob",
+    );
   }
   return blob.arrayBuffer();
 }
